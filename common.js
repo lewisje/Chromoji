b2b12f0ad9a3403756e841a93b862e8243f729f8
James Edward Lewis II <lewisje@alumni.iu.edu>
2015-03-13 01:38:37 -0400

    more code cleanup

--- common.js
+++ common.js
@@ -1,11 +1,9 @@
 function readCharDictionary(callback) {
   'use strict';
-  var request = new XMLHttpRequest(), path = 'chardict.json',
-   url = chrome.extension.getURL(path);
+  var request = new XMLHttpRequest(), url = chrome.extension.getURL('chardict.json');
   request.open('GET', url);
   request.addEventListener('load', function getChars(e) {
-    var chars = JSON.parse(request.responseText);
-    callback(chars);
+    callback(JSON.parse(request.responseText));
   }, false);
   request.send(null);
 }
