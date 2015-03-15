2b98fc82ff7a5bfcf5fc586e4ecefb48dd08306a
James Edward Lewis II <lewisje@alumni.iu.edu>
2015-03-14 23:39:08 -0400

    trying to get around jQuery.extend for object copy

--- background.js
+++ background.js
@@ -6,7 +6,15 @@ function setDefaultSettings() {
 
 function listenEmo(request, sender, sendResponse) {
   'use strict';
-  var response = $.extend(true, {}, request);
+  var clone = function clone(dest, strt) {
+    for (var prop in strt) {
+      if (strt.hasOwnProperty(prop)) {
+        if ((typeof strt[prop] === 'object' || typeof strt[prop] === 'function') &&
+            strt[prop] !== null && dest[prop]) clone(dest[prop], strt[prop]);
+        else dest[prop] = strt[prop];
+      }
+    }
+  }, response = clone(Object.create(Object.getPrototypeOf(request)), request);
   if (request.setting) response.result = localStorage[request.setting];
   sendResponse(response);
 }
